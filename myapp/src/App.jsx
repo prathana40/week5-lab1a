420fada01ac1ebe962430b6179bc7c556a42284a
Prathana <prathanay40@gmail.com>
2025-05-16 00:58:20 +0530

    first commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,14 @@
+ // App.jsx
+import Hello from './hello';
+import Bye from './bye';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
